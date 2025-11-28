994f5feb854918cd16c4086bfae7697edee6408d
raykido323-dotcom <raykido323@gmail.com>
2025-11-28 11:07:20 +0800

    exit

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "台北大巨蛋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.323102338348871,
+        "pitch": -0.2782303629302909,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.5600191699617696,
+          "pitch": -0.38180287919274214,
+          "title": "大巨蛋",
+          "text": "台北大巨蛋是台灣第一座大型「室內棒球場」，擁有可遮蔽天候的巨型屋蓋，無論颱風、下雨或大太陽都能進行活動。"
+        }
+      ]
+    }
+  ],
+  "name": "首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
